4f7ea4f3ec86e9e4febe7715df0bf0b290f2f89a
Vitor Freitas <vitorfs@gmail.com>
2015-03-25 22:58:41 +0200

    create base routes, working on templates

--- value/settings.py
+++ value/settings.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- value/urls.py
+++ value/urls.py
@@ -1,10 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'value.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'value.core.views.home', name='home'),
 )
