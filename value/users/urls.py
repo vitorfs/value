1be3a8babd90e74c18a7cbaaaf82634415f2946e
Vitor Freitas <vitorfs@gmail.com>
2015-03-30 12:41:20 +0300

    add basic form for user edit

--- value/users/urls.py
+++ value/users/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('value.users.views',
     url(r'^$', 'users', name='users'),
+    url(r'^(?P<username>[^/]+)/$', 'user', name='user'),
+    url(r'^(?P<username>[^/]+)/password/$', 'password', name='password'),
 )
--- value/users/views.py
+++ value/users/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
 
+@login_required
 def users(request):
     users = User.objects.all()
     return render(request, 'users/users.html', { 'users' : users })
+
+@login_required
+def user(request, username):
+    page_user = User.objects.get(username=username)
+    form = UserChangeForm(instance=page_user)
+    return render(request, 'users/user.html', { 'page_user' : page_user, 'form' : form })
+
+@login_required
+def password(request, username):
+    return render(request, 'users/users.html')
