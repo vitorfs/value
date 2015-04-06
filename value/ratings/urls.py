8c337985ea25ee79eceb562498694b15f8577d54
Vitor Freitas <vitorfs@gmail.com>
2015-04-06 22:39:57 +0300

    migrating rating model

--- value/ratings/urls.py
+++ value/ratings/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('value.ratings.views',
     url(r'^$', 'ratings', name='ratings'),
+    url(r'^add/$', 'add_rating', name='add_rating'),
 )
--- value/ratings/views.py
+++ value/ratings/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def ratings(request):
     return render(request, 'ratings/ratings.html')
+
+def add_rating(request):
+    return render(request, 'ratings/ratings.html')
