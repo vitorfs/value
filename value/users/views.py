690eb41ccfd6ec13bc78f196d7a893f5ec37fae9
Vitor Freitas <vitorfs@gmail.com>
2015-04-07 10:14:16 +0300

    add admin change password form

--- value/users/views.py
+++ value/users/views.py
@@ -2,7 +2,8 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AdminPasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 from django.forms.models import modelform_factory
 
 @login_required
@@ -31,12 +32,20 @@ def user(request, username):
         if form.is_valid():
             form.save()
             return redirect(reverse('users'))
-        else:
-            print form.errors
     else:
         form = Form(instance=page_user)
     return render(request, 'users/user.html', { 'page_user' : page_user, 'form' : form })
 
 @login_required
 def password(request, username):
-    return render(request, 'users/users.html')
+    page_user = User.objects.get(username=username)
+    if request.method == 'POST':
+        form = AdminPasswordChangeForm(page_user, request.POST)
+        if form.is_valid():
+            form.save()
+            if request.user == page_user:
+                update_session_auth_hash(request, form.user)
+            return redirect(reverse('user', args=(page_user.username,)))
+    else:
+        form = AdminPasswordChangeForm(page_user)
+    return render(request, 'users/password.html', { 'page_user' : page_user, 'form' : form })
