0cdade92a436cdb24b4e1979a83f00f0f23e8e34
Vitor Freitas <vitorfs@gmail.com>
2015-04-08 19:11:31 +0300

    working on scale edit

--- value/ratings/views.py
+++ value/ratings/views.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.forms.models import inlineformset_factory
 from value.ratings.models import Rating, RatingValue
@@ -12,7 +12,7 @@ def ratings(request):
 
 @login_required
 def add_rating(request):
-    RatingValueFormSet = inlineformset_factory(Rating, RatingValue, fields=('description', 'weight',))
+    RatingValueFormSet = inlineformset_factory(Rating, RatingValue, fields=('description', 'weight',), extra=1)
     if request.method == 'POST':
         form = RatingForm(request.POST)
         formset = RatingValueFormSet(request.POST)
@@ -26,4 +26,20 @@ def add_rating(request):
         rating = Rating()
         form = RatingForm(instance=rating)
         formset = RatingValueFormSet(instance=rating)
-    return render(request, 'ratings/add_rating.html', { 'form' : form, 'formset' : formset })
+    return render(request, 'ratings/rating.html', { 'form' : form, 'formset' : formset })
+
+@login_required
+def rating(request, rating_id):
+    rating = get_object_or_404(Rating, pk=rating_id)
+    RatingValueFormSet = inlineformset_factory(Rating, RatingValue, fields=('description', 'weight',), extra=1)
+    if request.method == 'POST':
+        form = RatingForm(request.POST, instance=rating)
+        formset = RatingValueFormSet(request.POST, instance=rating)
+        if form.is_valid() and formset.is_valid():
+            form.save()
+            formset.save()
+            return redirect(reverse('ratings'))
+    else:
+        form = RatingForm(instance=rating)
+        formset = RatingValueFormSet(instance=rating)
+    return render(request, 'ratings/rating.html', { 'form' : form, 'formset' : formset })
