76b742ed1d233c2c159be49b8b231acda41ed00f
Vitor Freitas <vitorfs@gmail.com>
2015-04-09 22:42:11 +0300

    add and remove values scripts

---
+++ value/ratings/static/js/ratings.js
@@ -0,0 +1,41 @@
+$(function () {
+
+  $(".js-add-value").click(function () {
+
+    var template = [
+      "<tr>",
+      "<td>",
+      "<input type='hidden' name='ratingvalue_set-#-id' id='id_ratingvalue_set-#-id'>",
+      "<input type='hidden' name='ratingvalue_set-#-rating' id='id_ratingvalue_set-#-rating'>",
+      "<input type='text' maxlength='255' name='ratingvalue_set-#-description' id='id_ratingvalue_set-#-description' value='' class='form-control'>",
+      "</td>",
+      "<td>",
+      "<input type='number' name='ratingvalue_set-#-weight' id='id_ratingvalue_set-#-weight' value='' class='form-control'>",
+      "</td>",
+      "<td>",
+      "<a href='#' class='js-remove-value'>",
+      "<span class='glyphicon glyphicon-remove-sign'></span>",
+      "</a>",
+      "</td>",
+      "</tr>"
+    ];
+
+    var count = $("table tbody tr").length;
+    var html = template.join("\n").replace(/#/g, count);
+    $("#id_ratingvalue_set-TOTAL_FORMS").val(count + 1);
+    $("table tbody").append(html);
+
+    return false;
+  });
+
+
+  $("table tbody").on("click", ".js-remove-value", function () {
+    $(this).closest("tr").remove();
+
+    var count = $("table tbody tr").length;
+    $("#id_ratingvalue_set-TOTAL_FORMS").val(count);
+
+    return false;
+  });
+
+});
