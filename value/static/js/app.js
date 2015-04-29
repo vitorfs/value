0485d101df74a556e3fdee511c1b5f7910c55941
Vitor Freitas <vitorfs@gmail.com>
2015-04-29 10:02:33 +0300

    add loading states to the app

--- value/static/js/app.js
+++ value/static/js/app.js
@@ -1,3 +1,13 @@
+var loading = function () {
+  if ($("body").hasClass("no-scroll")) {
+    $("body").removeClass("no-scroll");
+  }
+  else {
+    $("body").addClass("no-scroll");
+  }
+  $(".loading").toggle();
+};
+
 $(function () {
 
   // Active bootstrap popover plugin
