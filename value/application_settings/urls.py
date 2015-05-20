c9f48741d7e5b7c8a554736d639c0af0d3f8fdc5
Vitor Freitas <vitorfs@gmail.com>
2015-05-20 10:09:08 +0300

    working on the responsiveness of the horizontal forms. changed profile settings layout for a more consistent layout. working on application settings.

--- value/application_settings/urls.py
+++ value/application_settings/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('value.application_settings.views',
     url(r'^$', 'index', name='index'),
+    url(r'^items/$', 'items', name='items'),
+    url(r'^notifications/$', 'notifications', name='notifications'),
 )
--- value/application_settings/views.py
+++ value/application_settings/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
 
+@login_required
+@user_passes_test(lambda user: user.is_superuser)
 def index(request):
     admins = User.objects.filter(is_superuser=True)
     return render(request, 'application_settings/index.html', { 'admins': admins })
+
+@login_required
+@user_passes_test(lambda user: user.is_superuser)
+def items(request):
+    return render(request, 'application_settings/items.html')
+
+@login_required
+@user_passes_test(lambda user: user.is_superuser)
+def notifications(request):
+    return render(request, 'application_settings/notifications.html')
