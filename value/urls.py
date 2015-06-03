4564b7afd0db3d01032a4c95d8bff5c34771960c
Vitor Freitas <vitorfs@gmail.com>
2015-06-03 14:13:08 +0300

    add custom error pages

--- value/urls.py
+++ value/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
+
 urlpatterns = patterns('',
     url(r'^$', 'value.core.views.home', name='home'),
     url(r'^signin/$', 'django.contrib.auth.views.login', { 'template_name' : 'core/signin.html' }, name='signin'),
