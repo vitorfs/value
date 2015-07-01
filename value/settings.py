36c2d5a64608c1600b2171cd78800866505099b8
Vitor Freitas <vitorfs@gmail.com>
2015-07-01 10:20:07 +0300

    update settings.py

--- value/settings.py
+++ value/settings.py
@@ -32,7 +32,7 @@ EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.
 EMAIL_FILE_PATH = PROJECT_DIR.parent.parent.child('maildumps')
 
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', 'valueproject.fi',]
 
 
 INSTALLED_APPS = (
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     #'debug_toolbar',
 
     'value.core',
