86b0ec9d7dac4c264d49f3eca35ccdf0093c201d
Vitor Freitas <vitorfs@gmail.com>
2015-10-08 19:48:42 +0300

    Update script

--- install.sh
+++ install.sh
@@ -13,9 +13,11 @@ sudo apt-get -y install python-virtualenv
 IP_ADDRESS="$(curl ipecho.net/plain)"
 DATABASE_PASSWORD="$(pwgen 32 1)"
 
-su postgres -c "createuser u_value"
-su postgres -c "psql -c \"ALTER USER u_value WITH PASSWORD '${DATABASE_PASSWORD}';\"" --preserve-environment
-su postgres -c "createdb --owner u_value value"
+sudo -u postgres bash << EOF
+createuser u_value
+psql -c "ALTER USER u_value WITH PASSWORD '${DATABASE_PASSWORD}';"
+createdb --owner u_value value
+EOF
 
 sudo groupadd --system webapps
 sudo useradd --system --gid webapps --shell /bin/bash --home /webapps/value_tool value
@@ -23,17 +25,20 @@ sudo useradd --system --gid webapps --shell /bin/bash --home /webapps/value_tool
 sudo mkdir -p /webapps/value_tool/
 sudo chown value /webapps/value_tool/
 
-su value -c "virtualenv /webapps/value_tool/"
-su value -c "source /webapps/value_tool/bin/activate"
+sudo -u value bash << EOF
+virtualenv /webapps/value_tool/
+EOF
 
 cp -r value/ /webapps/value_tool/value
 
+sudo -u value bash << EOF
+source /webapps/value_tool/bin/activate
 cp /webapps/value_tool/value/conf/gunicorn_start.bash /webapps/value_tool/bin/gunicorn_start
-
+sudo chmod u+x bin/gunicorn_start
 sudo chown -R value:users /webapps/value_tool
 sudo chmod -R g+w /webapps/value_tool
 
-su value -c "pip install -r /webapps/value_tool/value/requirements_prod.txt"
+pip install -r /webapps/value_tool/value/requirements_prod.txt
 
 SECRET_KEY="$(python /webapps/value_tool/value/scripts/generate_secret_key.py)"
 
@@ -42,23 +47,24 @@ sed -i -e 's@{SECRET_KEY}@${SECRET_KEY}@g' /webapps/value_tool/value/.env
 sed -i -e 's@{IP_ADDRESS}@${IP_ADDRESS}@g' /webapps/value_tool/value/.env
 sed -i -e 's@{DATABASE_PASSWORD}@${DATABASE_PASSWORD}@g' /webapps/value_tool/value/.env
 
-su value -c "python /webapps/value_tool/value/manage.py migrate"
-su value -c "python /webapps/value_tool/value/manage.py collectstatic --noinput"
+python /webapps/value_tool/value/manage.py migrate
+python /webapps/value_tool/value/manage.py collectstatic --noinput
+mkdir -p /webapps/value_tool/media/
 
-su value -c "mkdir -p /webapps/value_tool/media/"
+sudo cp /webapps/value_tool/value/conf/value.conf /etc/supervisor/conf.d/value.conf
+mkdir -p /webapps/value_tool/logs/
+touch /webapps/value_tool/logs/gunicorn_supervisor.log
 
-cp /webapps/value_tool/value/conf/value.conf /etc/supervisor/conf.d/value.conf
-su value -c "mkdir -p /webapps/value_tool/logs/"
-su value -c "touch /webapps/value_tool/logs/gunicorn_supervisor.log"
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl status value
 
-su value -c "sudo supervisorctl reread"
-su value -c "sudo supervisorctl update"
-su value -c "sudo supervisorctl status value"
-
-su value -c "sudo service nginx start"
-cp /webapps/value_tool/value/conf/value.nginxconf /etc/nginx/sites-available/value
+sudo service nginx start
+sudo cp /webapps/value_tool/value/conf/value.nginxconf /etc/nginx/sites-available/value
 sed -i -e 's@{IP_ADDRESS}@${IP_ADDRESS}@g' /etc/nginx/sites-available/value
 sudo ln -s /etc/nginx/sites-available/value /etc/nginx/sites-enabled/value
-su value -c "sudo service nginx restart"
+sudo service nginx restart
+
+EOF
 
 exit 0
