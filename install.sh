a8aac6b242734521f0854025b9f0c1e40f7787d5
Vitor Freitas <vitorfs@gmail.com>
2015-10-08 21:17:43 +0300

    Update script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+LB="\n********************************************************************************\n"
+
+printf "\n$LB UPDATING SYSTEM $LB"
 sudo apt-get update
 sudo apt-get -y upgrade
 
+printf "\n$LB INSTALLING APPLICATION SERVER DEPENDENCIES $LB"
 sudo apt-get -y install pwgen
 sudo apt-get -y install postgresql postgresql-contrib
 sudo apt-get -y install libpq-dev python-dev
@@ -10,98 +14,96 @@ sudo apt-get -y install supervisor
 sudo apt-get -y install nginx
 sudo apt-get -y install python-virtualenv
 
-LB="********************************************************************************\n"
-
-printf "$LB Getting public IP address $LB"
+printf "\n$LB GETTING PUBLIC IP ADDRESS $LB"
 IP_ADDRESS="$(curl ipecho.net/plain)"
 
-printf "$LB Generating random database password $LB"
+printf "\n$LB GENERATING RANDOM DATABASE PASSWORD $LB"
 DATABASE_PASSWORD="$(pwgen 32 1)"
 
-printf "$LB Creating postgresql database $LB"
+printf "\n$LB CREATING POSTGRESQL DATABASE $LB"
 su postgres -c "createuser u_value"
 su postgres -c "psql -c \"ALTER USER u_value WITH PASSWORD '${DATABASE_PASSWORD}';\""
 su postgres -c "createdb --owner u_value value"
 
-printf "$LB Creating application group and user $LB"
+printf "\n$LB CREATING APPLICATION GROUP AND USER $LB"
 sudo groupadd --system webapps
 sudo useradd --system --gid webapps --shell /bin/bash --home /webapps/value_tool value
 
-printf "$LB Creating VALUE Tool folder $LB"
+printf "\n$LB CREATING VALUE TOOL FOLDER $LB"
 sudo mkdir -p /webapps/value_tool/
 
-printf "$LB Assigning user value to application folder $LB"
+printf "\n$LB ASSIGNING USER VALUE TO APPLICATION FOLDER $LB"
 sudo chown value /webapps/value_tool/
 
-printf "$LB Starting virtual env $LB"
+printf "\n$LB STARTING VIRTUAL ENV $LB"
 virtualenv /webapps/value_tool/
 
-printf "$LB Coping source code to virtual env $LB"
+printf "\n$LB COPING SOURCE CODE TO VIRTUAL ENV $LB"
 cp -r value/ /webapps/value_tool/value
 
-printf "$LB Starting virtual env $LB"
+printf "\n$LB STARTING VIRTUAL ENV $LB"
 source /webapps/value_tool/bin/activate
 
-printf "$LB Copying gunicorn initialization script $LB"
+printf "\n$LB COPYING GUNICORN INITIALIZATION SCRIPT $LB"
 cp /webapps/value_tool/value/conf/gunicorn_start.bash /webapps/value_tool/bin/gunicorn_start
 
-printf "$LB Add execute bit to gunicorn_start $LB"
+printf "\n$LB ADD EXECUTE BIT TO GUNICORN_START $LB"
 sudo chmod u+x /webapps/value_tool/bin/gunicorn_start
 
-printf "$LB Installing project dependencies $LB"
+printf "\n$LB INSTALLING PROJECT DEPENDENCIES $LB"
 pip install -r /webapps/value_tool/value/requirements_prod.txt
 
-printf "$LB Generating application secret key $LB"
+printf "\n$LB GENERATING APPLICATION SECRET KEY $LB"
 SECRET_KEY="$(python /webapps/value_tool/value/scripts/generate_secret_key.py)"
 
-printf "$LB Creating application env file $LB"
+printf "\n$LB CREATING APPLICATION ENV FILE $LB"
 cp /webapps/value_tool/value/conf/app_env /webapps/value_tool/value/.env
 
-printf "$LB Add instance specific secret key, ip address, database password $LB"
+printf "\n$LB ADD INSTANCE SPECIFIC SECRET KEY, IP ADDRESS, DATABASE PASSWORD $LB"
 sed -i "s/{SECRET_KEY}/$SECRET_KEY/" /webapps/value_tool/value/.env
 sed -i "s/{IP_ADDRESS}/$IP_ADDRESS/" /webapps/value_tool/value/.env
 sed -i "s/{DATABASE_PASSWORD}/$DATABASE_PASSWORD/" /webapps/value_tool/value/.env
 
-printf "$LB Generate application database $LB"
+printf "\n$LB GENERATE APPLICATION DATABASE $LB"
 python /webapps/value_tool/value/manage.py migrate
 
-printf "$LB Collect static files $LB"
+printf "\n$LB COLLECT STATIC FILES $LB"
 python /webapps/value_tool/value/manage.py collectstatic --noinput
 
-printf "$LB Creating media folder $LB"
+printf "\n$LB CREATING MEDIA FOLDER $LB"
 mkdir -p /webapps/value_tool/media/
 
-printf "$LB Copying supervisor script $LB"
+printf "\n$LB COPYING SUPERVISOR SCRIPT $LB"
 sudo cp /webapps/value_tool/value/conf/value.conf /etc/supervisor/conf.d/value.conf
 
-printf "$LB Creating application log folder $LB"
+printf "\n$LB CREATING APPLICATION LOG FOLDER $LB"
 mkdir -p /webapps/value_tool/logs/
 touch /webapps/value_tool/logs/gunicorn_supervisor.log
 
-printf "$LB Change application folder rights to value user $LB"
+printf "\n$LB CHANGE APPLICATION FOLDER RIGHTS TO VALUE USER $LB"
 sudo chown -R value:users /webapps/value_tool
 sudo chmod -R g+w /webapps/value_tool
 
-printf "$LB Hook application to supervisor $LB"
+printf "\n$LB HOOK APPLICATION TO SUPERVISOR $LB"
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl status value
 
-printf "$LB Starting NGINX $LB"
+printf "\n$LB STARTING NGINX $LB"
 sudo service nginx start
 
-printf "$LB Copying configuration files $LB"
+printf "\n$LB COPYING CONFIGURATION FILES $LB"
 sudo cp /webapps/value_tool/value/conf/value.nginxconf /etc/nginx/sites-available/value
 
-printf "$LB Update IP address $LB"
+printf "\n$LB UPDATE IP ADDRESS $LB"
 sed -i "s/{IP_ADDRESS}/${IP_ADDRESS}/" /etc/nginx/sites-available/value
 
-printf "$LB Enable site $LB"
+printf "\n$LB ENABLE SITE $LB"
 sudo ln -s /etc/nginx/sites-available/value /etc/nginx/sites-enabled/value
 
-printf "$LB Restarting NGINX $LB"
+printf "\n$LB RESTARTING NGINX $LB"
 sudo service nginx restart
 
-printf "$LB SUCCESS! $LB"
+printf "\n$LB SUCCESS! $LB"
 
 exit 0
