2896056f6997db358931c772e1fa4016b842792c
Vitor Freitas <vitorfs@gmail.com>
2018-05-02 10:10:16 +0300

    Add fi locale. Update pt_BR

--- value/settings.py
+++ value/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,7 +92,8 @@ USE_TZ = True
 
 LANGUAGES = (
     ('en-us', 'English'),
-    #('pt-br', 'Portuguese')
+    ('pt-br', 'Portuguese'),
+    ('fi', 'Finnish'),
 )
 
 LOCALE_PATHS = (
