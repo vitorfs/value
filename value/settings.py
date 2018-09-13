74518953ac67cf814aa7b177c0ecc5b208a1db86
Vitor Freitas <vitorfs@gmail.com>
2018-09-13 17:18:48 +0300

    Update Finnish translations

--- value/settings.py
+++ value/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'value.urls'
