7c026911655e39e68148268b3ae1f060077bb10b
Vitor Freitas <vitorfs@gmail.com>
2018-11-14 12:46:24 +0200

    Fix context processor

--- value/deliverables/context_processors.py
+++ value/deliverables/context_processors.py
@@ -4,11 +4,12 @@ from .models import Deliverable
 def deliverable_admin(request):
     context = {'is_deliverable_admin': False}
     if hasattr(request, 'resolver_match'):
-        deliverable_id = request.resolver_match.kwargs.get('deliverable_id')
-        try:
-            deliverable = Deliverable.objects.get(pk=deliverable_id)
-            if deliverable.manager == request.user or deliverable.admins.filter(pk=request.user.pk).exists():
-                context['is_deliverable_admin'] = True
-        except Deliverable.DoesNotExist:
-            pass
+        if request.resolver_match is not None:
+            deliverable_id = request.resolver_match.kwargs.get('deliverable_id')
+            try:
+                deliverable = Deliverable.objects.get(pk=deliverable_id)
+                if deliverable.manager == request.user or deliverable.admins.filter(pk=request.user.pk).exists():
+                    context['is_deliverable_admin'] = True
+            except Deliverable.DoesNotExist:
+                pass
     return context
